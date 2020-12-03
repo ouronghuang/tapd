3bc523875c7efaf8ade0d8f0665352012ea9a0de
欧荣凰 <ouronghuang@dingtalk.com>
2020-12-03 09:52:22 +0800

    feat(测试): 基类测试

--- tests/TapdTest.php
+++ tests/TapdTest.php
@@ -2,10 +2,31 @@
 
 namespace Orh\Tapd\Tests;
 
+use Orh\Tapd\Exceptions\{InvalidModuleException, NullException};
+use Orh\Tapd\Modules\Base;
+
 class TapdTest extends TestCase
 {
-    public function testBasic()
+    public function testInvalidModule()
     {
-        $this->assertTrue(true);
+        $this->expectException(InvalidModuleException::class);
+        $this->expectExceptionMessage('Undefined module: mock.');
+        $this->tapd->mock;
+    }
+
+    public function testNullHttp()
+    {
+        $this->expectException(NullException::class);
+        $this->expectExceptionMessage('Http attribute is null, please set http first.');
+        $this->tapd->story;
+    }
+
+    public function testUseModule()
+    {
+        $apiUser = 'mock-user';
+        $apiPassword = 'mock-password';
+        $this->tapd->setHttp($apiUser, $apiPassword);
+
+        $this->assertTrue($this->tapd->story instanceof Base);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,17 @@
 
 namespace Orh\Tapd\Tests;
 
+use Orh\Tapd\Tapd;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 class TestCase extends BaseTestCase
 {
+    protected $tapd = null;
 
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->tapd = new Tapd();
+    }
 }
